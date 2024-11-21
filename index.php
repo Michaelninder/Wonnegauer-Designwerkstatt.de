14182afd8acae7d40fc89193f7258896810b298e
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2024-11-21 13:41:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include 'assets/php/config.php';
-${page.name} = ${name.index}; ${page.title} = ${title.index}; ${page.message} = ${message.index};
+${page.name} = ${name.index}; 
+${page.title} = ${title.index}; ${page.message} = ${message.index};
 
 
