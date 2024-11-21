cbf95b7bc17f104a6c8e3a8af1657161699ff0a5
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2024-11-21 13:38:49 +0100

    Create header.php

---
+++ assets/php/header.php
@@ -0,0 +1,14 @@
+<body>
+<header>
+  <nav>
+    <ul>
+      <li><a href="index.php">Startseite</a></li>
+      <li><a href="contact.php">Kontakt</a></li>
+      <li><a href="design.php">Design</a></li>
+      <li><a href="art.php">Kunst</a></li>
+      <li><a href="current.php">Aktuelles</a></li>
+      <li><a href="tours.php">Führungen</a></li>
+      <li><a href="us.php">Wir</a></li>
+    </ul>
+  </nav>
+</header>
