dd1647b5c5161c00cf33ac7060820d03419fa8ac
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2024-11-21 13:50:44 +0100

    Update footer.php

--- assets/php/footer.php
+++ assets/php/footer.php
@@ -1,6 +1,18 @@
+
 <footer>
-  <p>&copy; <?php echo date("Y"); ?> Wonnegauer Designwerkstatt. Alle Rechte vorbehalten.</p>
-  <p><a href="impressum.php">Impressum</a> | <a href="datenschutz.php">Datenschutz</a></p>
+  <div align="center">
+    <table border="0" cellpadding="0" cellspacing="0" width="955">
+      <tr>
+        <td width="754" valign="top"><font face="Consolas" size="2">WONNEGAUER DESIGNWERKSTATT Plenzer 6, 67592 Flörsheim-Dalsheim, Telefon: 06243/5649</font></td>
+        <td valign="top">
+          <p>&copy; <?php echo date("Y"); ?> Wonnegauer Designwerkstatt. Alle Rechte vorbehalten.</p>
+          <p align="right"><font face="Consolas">
+            <a href="impressum.php" style="text-decoration: none; letter-spacing: 2px">Impressum/Datenschutz</a>
+          </font></p>
+        </td>
+      </tr>
+    </table>
+  </div>
 </footer>
 </body>
 </html>
