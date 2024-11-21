c9e20afbf3c4b774885f068b7317dabad494484b
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2024-11-21 13:47:26 +0100

    Create footer.php

---
+++ assets/php/footer.php
@@ -0,0 +1,6 @@
+<footer>
+  <p>&copy; <?php echo date("Y"); ?> Wonnegauer Designwerkstatt. Alle Rechte vorbehalten.</p>
+  <p><a href="impressum.php">Impressum</a> | <a href="datenschutz.php">Datenschutz</a></p>
+</footer>
+</body>
+</html>
