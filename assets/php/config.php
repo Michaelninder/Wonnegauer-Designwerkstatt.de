2649d3cdf471bd3723e0162b83f3685f421971e1
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2024-11-21 11:57:03 +0100

    Update config.php

--- assets/php/config.php
+++ assets/php/config.php
@@ -2,7 +2,7 @@
 ${title.index} = "Startseite";
 ${title.contact} = "Kontakt";
 ${title.design} = "Design";
-${title.art.title} = "Kunst";
+${title.art} = "Kunst";
 ${current.title} = "Aktuelles";
 ${tours.title} = "Führungen";
 ${us.title} = "Wir";
