7544be3a5aa8d741b127d3350e56b1e2bd93cb15
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2024-11-21 15:40:37 +0100

    Update header.php

--- assets/php/header.php
+++ assets/php/header.php
@@ -1,23 +1,25 @@
 <header>
-  <div align="center">
-    <table border="0" cellpadding="0" cellspacing="0" width="955" height="80">
-      <tr>
-        <td colspan="2" background="assets/img/banner1.jpg">&nbsp;</td>
-      </tr>
-      <tr>
-        <td width="754">
-          <nav>
-            <p><b>
-              <a href="index.php" style="text-decoration: none">STARTSEITE</a>&nbsp;
-              <a href="design.php" style="text-decoration: none">DESIGN</a>&nbsp;
-              <a href="kunst.php" style="text-decoration: none">KUNST</a>&nbsp;
-              <a href="kultur.php" style="text-decoration: none">KULTUR</a>&nbsp;
-              <a href="gaestefuehrungen.php" style="text-decoration: none">GÄSTEFÜHRUNGEN</a>&nbsp;
-              <a href="kontakt.php" style="text-decoration: none">KONTAKT</a>
-            </b></p>
-          </nav>
-        </td>
-      </tr>
-    </table>
-  </div>
+    <nav>
+        <ul>
+            <li><a href="index.php"><?php echo $title['index']; ?></a></li>
+            <li><a href="contact.php"><?php echo $title['contact']; ?></a></li>
+            <li><a href="design.php"><?php echo $title['design']; ?></a></li>
+            <li><a href="art.php"><?php echo $title['art']; ?></a></li>
+            <li><a href="current.php"><?php echo $title['current']; ?></a></li>
+            <li><a href="tours.php"><?php echo $title['tours']; ?></a></li>
+            <li><a href="us.php"><?php echo $title['us']; ?></a></li>
+        </ul>
+        <div>
+            <button onclick="toggleTheme()">Toggle Theme</button>
+        </div>
+    </nav>
 </header>
+
+<script>
+    function toggleTheme() {
+        let currentTheme = document.cookie.replace(/(?:(?:^|.*;\s*)theme\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        let newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+        document.cookie = "theme=" + newTheme + "; path=/";
+        location.reload();
+    }
+</script>
