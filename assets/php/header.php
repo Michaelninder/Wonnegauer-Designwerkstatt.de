2111772d045497e00a7635ac81a7aab4963924ec
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2024-11-21 13:49:44 +0100

    Update header.php

--- assets/php/header.php
+++ assets/php/header.php
@@ -1,13 +1,23 @@
 <header>
-  <nav>
-    <ul>
-      <li><a href="index.php">Startseite</a></li>
-      <li><a href="contact.php">Kontakt</a></li>
-      <li><a href="design.php">Design</a></li>
-      <li><a href="art.php">Kunst</a></li>
-      <li><a href="current.php">Aktuelles</a></li>
-      <li><a href="tours.php">Führungen</a></li>
-      <li><a href="us.php">Wir</a></li>
-    </ul>
-  </nav>
+  <div align="center">
+    <table border="0" cellpadding="0" cellspacing="0" width="955" height="80">
+      <tr>
+        <td colspan="2" background="assets/img/banner1.jpg">&nbsp;</td>
+      </tr>
+      <tr>
+        <td width="754">
+          <nav>
+            <p><b>
+              <a href="index.php" style="text-decoration: none">STARTSEITE</a>&nbsp;
+              <a href="design.php" style="text-decoration: none">DESIGN</a>&nbsp;
+              <a href="kunst.php" style="text-decoration: none">KUNST</a>&nbsp;
+              <a href="kultur.php" style="text-decoration: none">KULTUR</a>&nbsp;
+              <a href="gaestefuehrungen.php" style="text-decoration: none">GÄSTEFÜHRUNGEN</a>&nbsp;
+              <a href="kontakt.php" style="text-decoration: none">KONTAKT</a>
+            </b></p>
+          </nav>
+        </td>
+      </tr>
+    </table>
+  </div>
 </header>
