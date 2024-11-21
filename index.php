a528934a493a1b7517fdc6dfa7fd991322c776f2
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2024-11-21 15:41:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php 
+$page_name = "index"; 
 include 'assets/php/config.php'; 
-include 'assets/php/cookies.php';
+include 'assets/php/cookies.php'; 
 
-// Setzt Seiteneigenschaften
-$page_name = "index";
-$page_title = $title[$page_name];
-$page_message = $message[$page_name];
+// Set the title and message dynamically
+$title = $title[$page_name]; 
+$message = $message[$page_name]; 
 
-// Kopfbereich einbinden
 include 'assets/php/head.php'; 
 ?>
 
@@ -15,8 +14,8 @@ include 'assets/php/head.php';
     <table border="0" cellpadding="0" cellspacing="0" width="955">
         <tr>
             <td valign="top" style="padding-left: 4px" colspan="3">
-                <h1><?php echo $page_title; ?></h1>
-                <p><?php echo $page_message; ?></p>
+                <h1><?php echo $title; ?></h1>
+                <p><?php echo $message; ?></p>
                 <p>Wir, Brigitte und Wolfgang Ternis, sind in den Bereichen Design, Kunst, Kultur und Tourismus in Rheinhessen tätig. Unser Ziel ist die Förderung von Kreativität, Lebensqualität und Wohlbefinden.</p>
             </td>
         </tr>
@@ -28,14 +27,7 @@ include 'assets/php/head.php';
                 <img src="assets/img/lyon.JPG" width="300" height="300" alt="Bild">
             </td>
         </tr>
-        <tr>
-            <td colspan="3" align="center">
-                <p>Gästeführungen, Fahrradführungen, Radwanderungen</p>
-            </td>
-        </tr>
     </table>
 </div>
 
-<?php 
-include 'assets/php/footer.php'; 
-?>
+<?php include 'assets/php/footer.php'; ?>
