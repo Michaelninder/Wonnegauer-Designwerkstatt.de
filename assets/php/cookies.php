551f508d8230356fe8c3d047ef33ec403e810efc
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-13 08:07:46 +0200

    Delete assets directory

--- assets/php/cookies.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Cookie für den Theme-Modus setzen, falls nicht gesetzt
-if (isset($_GET['theme'])) {
-    $theme = $_GET['theme'] === 'dark' ? 'dark' : 'light';
-    setcookie('theme', $theme, time() + 3600 * 24 * 30, '/');  
-}
-?>
--- assets/php/theme.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (isset($_GET['theme'])) {
-    $theme = $_GET['theme'] === 'dark' ? 'dark' : 'light';
-    setcookie('theme', $theme, time() + 3600 * 24 * 30, '/');  // Setze Cookie für 30 Tage
-}
-header("Location: " . $_SERVER['HTTP_REFERER']);
-exit;
-?>
