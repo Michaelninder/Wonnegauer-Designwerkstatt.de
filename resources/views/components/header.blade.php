643dad458eeffd8dc5e6f8cddb02a495912ad679
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-13 07:51:47 +0200

    Create header.blade.php

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,11 @@
+<header>
+  
+    <div class="container-fluid p-0">
+        <div class="row">
+            <div class="col-12 p-0" style="background-image: url('{{ asset(path: 'img/banner1.jpg') }}'); background-size: contain; background-repeat: no-repeat; height: 96px; background-position: center center;">
+            </div>
+        </div>
+    </div>
+
+    @include('components.navbar')
+</header>
