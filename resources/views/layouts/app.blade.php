ac9d7e019cbf1a521d635dfe28c00aaf6723dbeb
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-13 07:49:33 +0200

    Create app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wonnegauer-Designwerkstatt</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+
+</head>
+<body>
+    @yield('layout')
+    @yield('direct')
+</body>
+</html>
