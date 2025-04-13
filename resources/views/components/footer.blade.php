fef05304ce546de13419981a8768ee5fe18da863
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-13 07:51:23 +0200

    Create footer.blade.php

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,19 @@
+<footer class="bg-dark text-white py-4 mt-5">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-8">
+          <p class="mb-1 fw-bold">WONNEGAUER DESIGNWERKSTATT</p>
+          <p class="mb-0">
+            Plenzer 6, 67592 Flörsheim-Dalsheim<br>
+            Telefon: 06243/5649
+          </p>
+        </div>
+        <div class="col-md-4 text-md-end mt-3 mt-md-0">
+          <a href="{{ route('legal.imprint') }}" class="text-white text-decoration-underline">
+            Impressum
+          </a>
+        </div>
+      </div>
+    </div>
+</footer>
+  
